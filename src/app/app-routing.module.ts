08be78925a0ab2e30286dce316b48df873c664b0
Edgar Butwilowski <edgar.butwilowski@win.ch>
2023-07-07 12:13:06 +0200

    Events can now be opened

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,19 +18,21 @@ import { ActivityAttributesComponent } from './activity-attributes/activity-attr
 import { ChooseEventsComponent } from './choose-events/choose-events.component';
 import { ConfigurationComponent } from './configuration/configuration.component';
 import { TestopenidComponent } from './testopenid/testopenid.component';
+import { EventAttributesComponent } from './event-attributes/event-attributes.component';
  
  const routes: Routes = [
    {path: '', component: WelcomeComponent, pathMatch: 'full'},
    {path: 'report', component: ReportComponent, pathMatch: 'full', canActivate: [UserService]},
-   {path: 'chooseneed', component: ChooseNeedComponent, pathMatch: 'full', canActivate: [UserService]},
-   {path: 'chooseactivity', component: ChooseActivityComponent, pathMatch: 'full', canActivate: [UserService]},
-   {path: 'need/:id', component: NeedAttributesComponent, pathMatch: 'full', canActivate: [UserService]},
-   {path: 'activity/:id', component: ActivityAttributesComponent, pathMatch: 'full', canActivate: [UserService]},
+   {path: 'needs', component: ChooseNeedComponent, pathMatch: 'full', canActivate: [UserService]},
+   {path: 'activities', component: ChooseActivityComponent, pathMatch: 'full', canActivate: [UserService]},
+   {path: 'needs/:id', component: NeedAttributesComponent, pathMatch: 'full', canActivate: [UserService]},
+   {path: 'activities/:id', component: ActivityAttributesComponent, pathMatch: 'full', canActivate: [UserService]},
    {path: 'users', component: UsersComponent, pathMatch: 'full', canActivate: [UserService]},
    {path: 'users/:email', component: UserComponent, pathMatch: 'full', canActivate: [UserService]},
    {path: 'managementareas', component: ManagementAreasComponent, pathMatch: 'full', canActivate: [UserService]},
    {path: 'login', component: LoginComponent, pathMatch: 'full'},
    {path: 'events', component: ChooseEventsComponent, pathMatch: 'full', canActivate: [UserService]},
+   {path: 'events/:id', component: EventAttributesComponent, pathMatch: 'full', canActivate: [UserService]},
    {path: 'configuration', component: ConfigurationComponent, pathMatch: 'full', canActivate: [UserService]},
    {path: 'openid', component: TestopenidComponent, pathMatch: 'full'},
  ];
--- src/model/event-properties.ts
+++ src/model/event-properties.ts
@@ -9,4 +9,5 @@ export class EventProperties {
     lastModified: Date = new Date(1, 0, 1);
     dateFrom: Date = new Date(1, 0, 1);
     dateTo: Date = new Date(1, 0, 1);
+    isEditingAllowed: boolean = false;
 }
