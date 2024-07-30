78194f4c70d5df0d730b8b2e1e6b6af93f29595d
Edgar Butwilowski <edgar.butwilowski@win.ch>
2024-07-30 14:06:41 +0200

    Added button 'Wiedereröffnen' to roadwork activity consultation process

--- src/app/activity-attributes/activity-attributes.component.ts
+++ src/app/activity-attributes/activity-attributes.component.ts
@@ -374,7 +374,6 @@ export class ActivityAttributesComponent implements OnInit {
   onRoadWorkActivityEnumChange() {
     if (this.roadWorkActivityFeature && this.roadWorkActivityFeature.properties.uuid) {
       this.roadWorkActivityFeature.properties.projectType = this.roadWorkActivityProjectTypeEnumControl.value;
-      this.save();
     }
   }
 
