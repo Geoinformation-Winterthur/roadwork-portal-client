ce5255db07692951f4c390fda7571a2e3a534f93
Edgar Butwilowski <edgar.butwilowski@win.ch>
2024-10-21 17:06:39 +0200

    Neue Version 2024.26 gesetzt

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -29,7 +29,7 @@ export class WelcomeComponent implements OnInit {
   userService: UserService;
 
   roadWorkActivityService: RoadWorkActivityService;
-  appVersion: string = "2024.25";
+  appVersion: string = "2024.26";
 
   displayedColumns: string[] = ['name', 'manager', 'created', 'period'];
 
