1113ba80fc3d7de7538e8dabe94798af57f3da70
Edgar Butwilowski <edgar.butwilowski@win.ch>
2024-10-23 10:05:42 +0200

    Added time to roadwork activity history

--- src/app/activity-history/activity-history.component.ts
+++ src/app/activity-history/activity-history.component.ts
@@ -11,11 +11,25 @@ export class ActivityHistoryComponent implements OnInit {
   @Input()
   dataSource: ActivityHistoryItem[] = [];
 
-  displayedColumns: string[] = ['when', 'who', 'what', 'comment'];
+  displayedColumns: string[] = ['date', 'time', 'person', 'activity', 'comment'];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource = this.dataSource;
+    let lastChangeDate: Date | undefined;
+    for (let activityHistoryItem of this.dataSource) {
+      let currChangeDate: Date | undefined;
+      if(activityHistoryItem.changeDate) currChangeDate = new Date(activityHistoryItem.changeDate);
+      if (lastChangeDate && currChangeDate) {
+        if (currChangeDate.getFullYear() == lastChangeDate.getFullYear() &&
+          currChangeDate.getMonth() == lastChangeDate.getMonth() &&
+          currChangeDate.getDay() == lastChangeDate.getDay()) {
+            activityHistoryItem.hideDate = true;
+        }
+      }
+      if(activityHistoryItem.changeDate) lastChangeDate = new Date(activityHistoryItem.changeDate);
+    }
   }
 
 }
