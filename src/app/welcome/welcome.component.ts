03dc41962a7f4df21da783086f54bba28d62bdbb
Edgar Butwilowski <edgar.butwilowski@win.ch>
2024-10-29 14:29:20 +0100

    Set new version 2024.27

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -37,7 +37,7 @@ export class WelcomeComponent implements OnInit {
 
   private roadWorkNeedService: RoadWorkNeedService;
   private roadWorkActivityService: RoadWorkActivityService;
-  appVersion: string = "2024.26";
+  appVersion: string = "2024.27";
 
   involvedOrgs: Map<string, OrganisationalUnit>;
 
