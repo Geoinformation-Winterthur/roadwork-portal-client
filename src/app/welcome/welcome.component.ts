1bdb3e025684d02df446991b0d25602ede8cf5de
Edgar Butwilowski <edgar.butwilowski@win.ch>
2025-04-23 09:57:20 +0200

    Versionsnummer auf 2025.10 erhöht

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -38,7 +38,7 @@ export class WelcomeComponent implements OnInit {
 
   private roadWorkNeedService: RoadWorkNeedService;
   private roadWorkActivityService: RoadWorkActivityService;
-  appVersion: string = "2025.9";
+  appVersion: string = "2025.10";
 
   involvedOrgs: Map<string, OrganisationalUnit>;
 
