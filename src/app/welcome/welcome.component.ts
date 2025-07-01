8df28c48c826a6c7173a085104039151ed8ae3a8
Edgar Butwilowski <edgar.butwilowski@win.ch>
2025-07-01 14:50:23 +0000

    Bump version to 2025.15

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -41,7 +41,7 @@ export class WelcomeComponent implements OnInit {
 
   private roadWorkNeedService: RoadWorkNeedService;
   private roadWorkActivityService: RoadWorkActivityService;
-  appVersion: string = "2025.14.2";
+  appVersion: string = "2025.15";
 
   involvedOrgs: Map<string, OrganisationalUnit>;
 
