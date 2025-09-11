175ef7b26090f47c81820a904d9798305132f3c4
Edgar Butwilowski <edgar.butwilowski@win.ch>
2025-09-11 12:59:21 +0000

    Add additional test cases for TimeFactorHelper.calcTimeFactor. All tests are green

--- src/helper/time-factor-helper.spec.ts
+++ src/helper/time-factor-helper.spec.ts
@@ -57,4 +57,69 @@ describe('TimeFactorHelper.calcTimeFactor', () => {
     const compare = createNeed('2025-01-01', '2025-01-02', '2025-01-03');
     expect(TimeFactorHelper.calcTimeFactor(compare, primary)).toBe(4);
   });
+
+  // Primary need for first test suite:
+  const primaryNeed1 = createNeed('2027-06-30', '2028-06-30', '2032-06-30');
+
+  it('should return time factor 3 in Excel-Test 1 (Bedarf AEW)', () => {
+    const compareNeed = createNeed('2027-06-30', '2028-06-30', '2032-06-30');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed1)).toBe(3);
+  });
+
+  it('should return time factor 2 in Excel-Test 2 (Bedarf EC)', () => {
+    const compareNeed = createNeed('2029-02-01', '2029-02-01', '2031-10-31');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed1)).toBe(2);
+  });
+
+  it('should return time factor 3 in Excel-Test 3 (Bedarf KuBa)', () => {
+    const compareNeed = createNeed('2027-07-31', '2028-03-31', '2031-03-31');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed1)).toBe(3);
+  });
+
+  // Primary need for second test suite:
+  const primaryNeed2 = createNeed('2026-10-01', '2027-10-01', '2029-10-01');
+
+  it('should return time factor 3 in Excel-Test 4 (Bedarf AEW)', () => {
+    const compareNeed = createNeed('2026-10-01', '2027-10-01', '2029-10-01');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed2)).toBe(3);
+  });
+
+  it('should return time factor 2 in Excel-Test 5 (Bedarf EC)', () => {
+    const compareNeed = createNeed('2029-02-01', '2029-02-01', '2031-10-31');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed2)).toBe(2);
+  });
+
+  it('should return time factor 2 in Excel-Test 6 (Bedarf KuBa)', () => {
+    const compareNeed = createNeed('2027-03-31', '2028-03-31', '2031-03-31');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed2)).toBe(2);
+  });
+
+  it('should return time factor 3 in Excel-Test 7 (Bedarf APK)', () => {
+    const compareNeed = createNeed('2026-07-01', '2027-04-01', '2028-10-01');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed2)).toBe(3);
+  });
+
+  // Primary need for third test suite:
+  const primaryNeed3 = createNeed('2026-10-01', '2027-10-01', '2029-10-01');
+
+  it('should return time factor 3 in Excel-Test 8 (Bedarf AEW)', () => {
+    const compareNeed = createNeed('2026-10-01', '2027-10-01', '2029-10-01');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed3)).toBe(3);
+  });
+
+  it('should return time factor 2 in Excel-Test 9 (Bedarf EC)', () => {
+    const compareNeed = createNeed('2029-02-01', '2029-02-01', '2031-10-31');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed3)).toBe(2);
+  });
+
+  it('should return time factor 2 in Excel-Test 10 (Bedarf KuBa)', () => {
+    const compareNeed = createNeed('2027-03-31', '2028-03-31', '2031-03-31');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed3)).toBe(2);
+  });
+
+  it('should return time factor 3 in Excel-Test 11 (Bedarf APK)', () => {
+    const compareNeed = createNeed('2026-07-01', '2027-04-01', '2028-10-01');
+    expect(TimeFactorHelper.calcTimeFactor(compareNeed, primaryNeed3)).toBe(3);
+  });
+
 });
