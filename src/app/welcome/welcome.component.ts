8b5132efd1027b070fb439c30f1d742ee49def2b
watson-dr <dariusz.milczuk@win.ch>
2026-08-10 13:16:04 +0000

    Version 2026.9

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -41,7 +41,7 @@ export class WelcomeComponent implements OnInit {
 
   private roadWorkNeedService: RoadWorkNeedService;
   private roadWorkActivityService: RoadWorkActivityService;
-  appVersion: string = "2026.8";
+  appVersion: string = "2026.9";
 
   involvedOrgs: Map<string, OrganisationalUnit>;
 
